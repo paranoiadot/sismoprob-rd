ca14304f98738482c2fd66462be6efbddb8fa93b
Rubio <easy@easys-MacBook-Pro.local>
2026-07-31 16:44:39 -0400

    Restaurar app y mapa con PWA configurada

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'sismoprob-cache-v1';
+const urlsToCache = [
+  '/',
+  '/api/sismos'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
